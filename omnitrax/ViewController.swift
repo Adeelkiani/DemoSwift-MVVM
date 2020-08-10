e96dc3a4ca41d92f54ca8f17e4c61c9a143d18d9
Adeel Kiani <adeel.kiani@globewyze.com>
2020-08-10 16:45:17 +0500

    Initial Commit

---
+++ omnitrax/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  omnitrax
+//
+//  Created by Adeel Kiani on 10/08/2020.
+//  Copyright © 2020 Globewyze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
